e09fb7af6d2d57fe41f7f2b8544d70c5302b6e09
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-10-19 07:27:48 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,22 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+  getQuoteUrl: string;
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Owner'],
+  customerRoles: [],
+  tenantRoles: ['Owner', 'Admin'],
+  tenantName: 'Company',
+  applicationName: 'Book Story v1',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: [],
+  ownerAbilities: ['Manage user data', 'Manage company data', 'Manage book data', 'Manage subscription data'],
+  getQuoteUrl: 'https://app.roq.ai/proposal/19bd59a7-7078-4b27-ba11-e998ade7b383',
+};
